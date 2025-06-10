61024b5c5c1818f7b1c135be5fcb69b9626d7cfa
diego <diegocornejo958@gmail.com>
2025-06-09 20:26:13 -0600

    patch localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,24 @@ import { lastCheck } from "./consts/consts.js"
 import { ValidateWinner } from "./components/winner.jsx"
 
 export function App(){
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.x)
+  const [board, setBoard] = useState(()=>{
+    const localBoard = JSON.parse(window.localStorage.getItem('board'))
+    if(localBoard) return localBoard
+    else{ return Array(9).fill(null) }
+  })
+  const [turn, setTurn] = useState(()=>{
+    const localTurn = window.localStorage.getItem('turn')
+    if(localTurn) return localTurn
+    else{ return TURNS.x }
+  })
   const [winner, setWinner] = useState(null)
 
   const resetGame = () =>{
     setBoard(Array(9).fill(null))
     setTurn(TURNS.x)
     setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const updateBoard = (index) => {
@@ -23,10 +33,12 @@ export function App(){
     //update turn
     const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
     setTurn(newTurn)
+    window.localStorage.setItem('turn', newTurn)
     //actaulizar board
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
     //check winner
     const newWinner = checkWinner(newBoard)
     if(newWinner){
